2d17fc762f48c350b0027410108d1897e7e43b26
elemental-archon <139948268+elemental-archon@users.noreply.github.com>
2023-07-26 22:20:14 +0530

    Update dailyassignment1.cpp

--- dailyassignment1.cpp
+++ dailyassignment1.cpp
@@ -1,14 +1,15 @@
-#include<stdio.h>
+/*Generate a Random Character.*/
+#include<stdio.h> //include required header files
 #include<stdlib.h>
 #include<time.h>
 
 int main(void)
 {
-	char characters;
-	srand(time(NULL));
-	characters=rand() % (70-65+1)+65;
+	char characters; //declaring characters variable as char data type
+	srand(time(NULL)); //using seed with time as random character vary with our system time
+	characters=rand() % (70-65+1)+65; //using ASCII code where character will be vary in range A - F
 	
-	printf("the randamoly generated character is %c", characters);
+	printf("the randamoly generated character is %c", characters); //print the randomaly generated character
 	
 	return 0;
 }
